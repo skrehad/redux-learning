2edbaafde4e9db4bcdade2e731a8723f831fafd6
rehadhasan <rehadhasan664@gmail.com>
2025-01-02 03:35:43 +0600

    shadcn ui install

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counter/counterSlice";
+import logger from "./middlewares/logger";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
