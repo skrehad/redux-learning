f335ff1e346d76359575a33b23ce7bf53771bb6a
rehadhasan <rehadhasan664@gmail.com>
2025-01-01 20:47:52 +0600

    added store in react application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import "./App.css";
-
-function App() {}
+function App() {
+  return (
+    <div>
+      <h1>Counter with Redux</h1>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+    </div>
+  );
+}
 
 export default App;
