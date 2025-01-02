7801e6b60acd9e391a1e6f43dff3025a1d714f63
rehadhasan <rehadhasan664@gmail.com>
2025-01-03 03:44:04 +0600

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import routes from "./routes/route.tsx";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={routes}></RouterProvider>{" "}
     </Provider>
   </StrictMode>
 );
---
+++ src/routes/route.tsx
@@ -0,0 +1,5 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([{ path: "/", element: <App></App> }]);
+export default routes;
